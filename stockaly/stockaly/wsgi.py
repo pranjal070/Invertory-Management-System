b4f3197b3e55dd0942a110e9ae102de25c3d4ea7
pranjal070 <pranjalthaku07@gmail.com>
2026-07-25 15:03:41 +0530

    Add vercel.json and WSGI app reference

--- stockaly/stockaly/wsgi.py
+++ stockaly/stockaly/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockaly.settings')
 
 application = get_wsgi_application()
+
+# Add this line for Vercel compatibility:
+app = application
